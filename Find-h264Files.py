20eb8169f08919344fe662789f77ead3376d11b0
Graham Pinkston <graham.pinkston@gmail.com>
2023-04-10 21:15:09 -0500

    working find and probe functions

--- Find-h264Files.py
+++ Find-h264Files.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
 import os
-import sys
-import json
-
-inputPath = '/mnt/data/Media/Movies/'
-
-codec = 'h264'
-type = 'video'
-
-cmd = 'ffprobe -v quiet -show_streams -print_format json ' + inputPath
-output = os.popen(cmd).read()
-output = json.loads(output)
-
-for stream in output['streams']:
-	if stream['codec_name'] == codec and stream['codec_type'] == type:
-		print(inputPath)
-		sys.exit(0)
+import ffmpeg
+input_path = '/mnt/data/Media/Movies/'
+non_h265_roster = '/mnt/data/Media/Movies/nonH265Roster.txt'
+h265_roster = '/mnt/data/Media/Movies/h265Roster.txt'
+os.system('clear')
+if os.path.exists(h265_roster):
+	os.remove(h265_roster)
+if os.path.exists(non_h265_roster):
+	os.remove(non_h265_roster)
+for current_path, directories, file_names in os.walk(input_path):
+	for file_name in file_names:
+		file_size = os.path.getsize(current_path + '/' + file_name)
+		if file_size > 268435456:
+			probe_output = ffmpeg.probe(current_path + '/' + file_name)
+			for stream in probe_output['streams']:
+				if (stream['codec_type'] == 'video'):
+					if (stream['codec_name'] == 'hevc'):
+						with open(h265_roster, "a") as openFile:
+							openFile.write(current_path + '/' + file_name + "\n")
+					else:
+						with open(non_h265_roster, "a") as openFile:
+							openFile.write(current_path + '/' + file_name + "\n")
+print('---1---')
+os.system('cat ' + non_h265_roster)
+print('---2---')
+os.system('cat ' + h265_roster)
