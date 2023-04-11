ee9c37b4a176672f6d460ded55e83976dcbff188
Graham Pinkston <graham.pinkston@gmail.com>
2023-04-11 16:21:42 -0500

    added arguments

--- ConvertToH265.py
+++ ConvertToH265.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 import os
 from colorama import init, Fore, Back, Style
-inputFile = '/mnt/data/Media/Movies/Spies_Like_Us_(1985)/Spies_Like_Us_(1985).mp4'
-outputFile = '/mnt/data/Media/Movies/Spies_Like_Us_(1985)/test.mp4'
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("input", help="Full path to video file to convert to h265.")
+args = parser.parse_args()
+inputFile = '/mnt/data/Media/Movies/Spies_Like_Us_\(1985\)/Spies_Like_Us_\(1985\).mp4'
+outputFile = '/mnt/data/Media/Movies/Spies_Like_Us_\(1985\)/test.mp4'
 cmd = 'ffmpeg -i ' + inputFile + ' -c:v libx265 -vtag hvc1 ' + outputFile
 print(Style.BRIGHT + Back.BLACK + Fore.GREEN + outputFile)
 os.system(cmd)
