b9619d7c9de311d0cd0ead42ae4d8ddafc0013fa
Graham Pinkston <graham.pinkston@gmail.com>
2023-05-23 22:13:57 -0500

    need to fix line 33

--- Vilicus.py
+++ Vilicus.py
@@ -2,7 +2,6 @@
 import json
 import logging
 import sys
-import time
 import subprocess
 import os
 parameters = json.load(open('parameters.json'))
@@ -10,7 +9,8 @@ opsLog = parameters['log_parent_path'] + parameters['log_filename']
 movies_manifest_path = parameters['log_parent_path'] + parameters['movies_manifest_filename']
 if os.path.exists(parameters['log_parent_path']):
 	if os.path.exists(movies_manifest_path):
-		os.remove(movies_manifest_path)
+		#os.remove(movies_manifest_path)
+		print('remove me')
 else:
 	os.mkdir(parameters['log_parent_path'])
 logging.basicConfig(
@@ -23,13 +23,13 @@ logging.basicConfig(
 )
 logging.info('EXECUTION START')
 logging.info('Creating non-h265 movie manifest...')
-subprocess.run(["python3", "CreateConversionManifest.py"])
+#subprocess.run(["python3", "CreateConversionManifest.py"])
 logging.info('Non-h265 movie manifest created.')
 logging.info('Beginning ffmpeg converstions...')
 manifest_file = open(movies_manifest_path, 'r')
 lines = manifest_file.readlines()
 for line in lines:
-	logging.debug("ConvertToH265.py $(line)")
-	subprocess.run(["python3", "ConvertToH265.py $(line)"])
+	logging.debug("ConvertToH265.py " + str(line))
+	subprocess.run(["python3", "ConvertToH265.py ", line])
 logging.info('EXECUTION STOP')
 logging.info('******************************************************')
