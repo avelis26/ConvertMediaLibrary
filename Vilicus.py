e0e9a24b3a9ffc2619b904022998888257fc50a6
Graham Pinkston <graham.pinkston@gmail.com>
2023-05-25 13:17:30 -0500

    working

--- Vilicus.py
+++ Vilicus.py
@@ -9,7 +9,7 @@ opsLog = parameters['log_parent_path'] + parameters['log_filename']
 movies_manifest_path = parameters['log_parent_path'] + parameters['movies_manifest_filename']
 if os.path.exists(parameters['log_parent_path']):
 	if os.path.exists(movies_manifest_path):
-		#os.remove(movies_manifest_path)
+		os.remove(movies_manifest_path)
 		print('remove me')
 else:
 	os.mkdir(parameters['log_parent_path'])
@@ -23,7 +23,7 @@ logging.basicConfig(
 )
 logging.info('EXECUTION START')
 logging.info('Creating non-h265 movie manifest...')
-#subprocess.run(["python3", "CreateConversionManifest.py"])
+subprocess.run(["python3", "CreateConversionManifest.py"])
 logging.info('Non-h265 movie manifest created.')
 logging.info('Beginning ffmpeg converstions...')
 manifest_file = open(movies_manifest_path, 'r')
