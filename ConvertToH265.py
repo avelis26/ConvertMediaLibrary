aa66c1e9b265c609b23cd00b00a53b43b98363d6
Graham Pinkston <graham.pinkston@gmail.com>
2023-05-25 13:16:14 -0500

    working

--- ConvertToH265.py
+++ ConvertToH265.py
@@ -3,8 +3,8 @@ import argparse
 import json
 import logging
 import sys
-import time
 import os
+import re
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help="Full path to video file to convert to h265.")
 args = parser.parse_args()
@@ -19,13 +19,12 @@ logging.basicConfig(
 	]
 )
 logging.info('Converting ' + args.input + 'to h265...')
-
-/mnt/data/Media/Movies/Back_To_The_Future_Part_II_(1989)/Back_To_The_Future_Part_II_(1989).mp4
 outputFile = args.input
 size = len(outputFile)
-replacement = 'test.mkv'
+replacement = 'mkv'
 outputFile = outputFile.replace(outputFile[size - 3:], replacement)
 logging.debug(outputFile)
-cmd = 'ffmpeg -i ' + args.input + ' -c:v libx265 -vtag hvc1 ' + outputFile
+cmd = 'ffmpeg -i ' + re.escape(args.input) + ' -c:v libx265 -vtag hvc1 ' + re.escape(outputFile)
+logging.debug(cmd)
 os.system(cmd)
 logging.info('Conversion complete.')
