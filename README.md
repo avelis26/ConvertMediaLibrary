c4be330e4be14e2332e1b004178414d068e40b87
Graham Pinkston <graham.pinkston@gmail.com>
2023-06-17 11:27:06 -0500

    ChatGPT4 Improvements with raw script.

--- README.md
+++ README.md
@@ -1,15 +1,167 @@
-WIP -- Needs updates.
-
 # ConvertMediaLibrary
 
 Python scripts to automate converting all h264 videos to h265.
 
 ## Question Proposed To ChatGPT:
-* Rewrite this script using the ffmpeg submodule instead of subprocess: https://github.com/avelis26/ConvertMediaLibrary/blob/main/Vilicus.py
+Please review and suggest improvements for the following python script:
+```
+#!/usr/bin/env python3
+import json
+import logging
+import sys
+import time
+import os
+import ffmpeg
+import decimal
+total_before_filesize = []
+total_after_filesize = []
+conversion_counter = 0
+
+def main():
+	global conversion_counter
+	# Load parameters from json file and set vars.
+	try:
+		parameters = json.load(open('parameters.json'))
+		input_path = parameters['movies_parent_path']
+		opsLog = parameters['log_parent_path'] + parameters['log_filename']
+		movies_manifest_path = parameters['log_parent_path'] + parameters['movies_manifest_filename']
+		exitFile = parameters['log_parent_path'] + parameters['exit_filename']
+	except Exception as e:
+		print("ERROR01: ",e)
+		exit()
+	# Create working directory ~/vilicus/ if not exist, delete manifest if exists.
+	try:
+		if os.path.exists(parameters['log_parent_path']):
+			if os.path.exists(movies_manifest_path):
+				os.remove(movies_manifest_path)
+		else:
+			os.mkdir(parameters['log_parent_path'])
+	except Exception as e:
+		print("ERROR02: ",e)
+		exit()
+	# Set up logging to terminal and file.
+	try:
+		logging.basicConfig(
+			level=logging.DEBUG,
+			format="%(asctime)s [%(levelname)s] %(message)s",
+			handlers=[
+				logging.FileHandler(opsLog),
+				logging.StreamHandler(sys.stdout)
+			]
+		)
+		logging.info('******************************************************')
+		logging.info('EXECUTION START')
+		logging.debug('input_path:           ' + input_path)
+		logging.debug('movies_manifest_path: ' + movies_manifest_path)
+		logging.debug('opsLog:               ' + opsLog)
+		logging.debug('exitFile:             ' + exitFile)
+		logging.info('Creating non-h265 movie manifest...')
+	except Exception as e:
+		logging.error("ERROR03: " + str(e))
+		exit()
+	# Create non-h265 movie manifest.
+	try:
+		movie_list = []
+		for current_path, directories, file_names in os.walk(input_path):
+			for file_name in file_names:
+				file_size = os.path.getsize(current_path + '/' + file_name)
+				if file_size > parameters['min_file_size']:
+					try:
+						probe_output = ffmpeg.probe(current_path + '/' + file_name)
+						for stream in probe_output['streams']:
+							if (stream['codec_type'] == 'video'):
+								if (stream['codec_name'] != 'hevc'):
+									print(file_name)
+									movie_list.append(current_path + '/' + file_name)
+					except ffmpeg.Error as e:
+						logging.error("ERROR06: " + (current_path + '/' + file_name))
+						print(e.stderr)
+		movie_set = set(movie_list)
+		logging.info('Total Non-h265 Movies: ' + str(len(movie_set)))
+		with open(movies_manifest_path, 'w') as openFile:
+			for movie in movie_set:
+				openFile.write("%s\n" % movie)
+		logging.info('Non-h265 movie manifest created.')
+	except Exception as e:
+		logging.error("ERROR06: " + str(e))
+		exit()
+	softExit(exitFile)
+	# Read manifest and convert 1 movie at a time.
+	logging.info('Beginning ffmpeg converstions...')
+	manifest_file = open(movies_manifest_path, 'r')
+	lines = manifest_file.readlines()
+	for line in lines:
+		conversion_counter += 1
+		ConvertToH265(line)
+		softExit(exitFile)
+	logging.info('EXECUTION STOP')
+	logging.info('******************************************************')
+
+# Define exit function to gracefully exit if exit file is found.
+def softExit(exitFilePath):
+	if os.path.exists(exitFilePath):
+		logging.info('EXECUTION STOPPED BY USER')
+		logging.info('******************************************************')
+		exit()
+
+# Define function to rename source file, convert to h265, validate, delete source file.
+def ConvertToH265(sourceFilePath):
+	global conversion_counter
+	global total_before_filesize
+	global total_after_filesize
+	sourceFilePath = sourceFilePath.strip()
+	base = os.path.splitext(sourceFilePath)[0]
+	outputFile = base + '.mkv'
+	logging.debug(outputFile)
+	try:
+		os.rename(sourceFilePath, sourceFilePath + '.old')
+		ffmpeg.input(sourceFilePath + '.old').output(outputFile, vcodec="libx265", crf=28, acodec="copy").run()
+		time.sleep(2)
+		ffmpeg.input(outputFile).output("null", f="null").run()
+		logging.info("Video validation succeeded.")
+	except Exception as e:
+		logging.error("ERROR04: " + str(e))
+		exit()
+	try:
+		before_file_size = os.path.getsize(sourceFilePath + '.old')
+		after_file_size = os.path.getsize(outputFile)
+		total_before_filesize.append(before_file_size)
+		total_after_filesize.append(after_file_size)
+		total_difference = sum(total_before_filesize) - sum(total_after_filesize)
+		space_saved = decimal.Decimal(total_difference) / decimal.Decimal(1073741824)
+		space_saved = round(space_saved, 2)
+		logging.debug('Before Size: ' + str(before_file_size))
+		logging.debug('After Size:  ' + str(after_file_size))
+		logging.debug('Difference:  ' + str(before_file_size - after_file_size))
+		os.remove(sourceFilePath + '.old')
+	except Exception as e:
+		logging.error("ERROR05: " + str(e))
+		exit()
+	logging.info('Conversions complete: ' + str(conversion_counter))
+	logging.debug('Total Diff:  ' + str(total_difference))
+	logging.info('Gigabytes saved: ' + str(space_saved) + ' GBs')
+
+if __name__ == "__main__":
+	main()
+```
 
 ## ChatGPT Summary
-- In this updated version, the ffmpeg-python module is imported and used for probing media files, executing FFmpeg commands, and handling the conversion process. The subprocess module is no longer used.
-- Please make sure you have the ffmpeg-python module installed (pip install ffmpeg-python) before running the script.
+Here are some suggestions for improving the provided Python script:
+1. Use a consistent indentation style: Python convention recommends using 4 spaces for indentation. Ensure that all indented blocks are consistently indented with 4 spaces for better readability.
+2. Organize imports: It's a good practice to organize imports by grouping them into standard library imports, third-party library imports, and local imports. Import statements should be placed at the top of the script, immediately after the shebang line.
+3. Use context managers for file handling: Instead of using json.load(open('parameters.json')), consider using the with statement to ensure proper file handling. For example, with open('parameters.json') as f: parameters = json.load(f). This will automatically close the file when done and handle any exceptions that might occur during file operations.
+4. Avoid using global variables: It's generally recommended to avoid using global variables whenever possible. Instead, pass variables as function arguments and return values. This helps encapsulate functionality and makes the code easier to understand and maintain.
+5. Provide meaningful error messages: When catching exceptions, it's helpful to provide more specific error messages that indicate the cause of the error. For example, instead of printing "ERROR01" or "ERROR02", provide a descriptive message that explains the issue encountered.
+6. Use logging consistently: Make sure to use logging consistently throughout the script for all relevant log messages. Avoid mixing print statements with logging. If you're using logging for most of the script, it's better to use logging exclusively.
+7. Handle exceptions gracefully: When encountering an exception, it's generally better to log the error and handle it gracefully rather than using exit(). Exiting abruptly may leave resources unclosed and prevent proper cleanup. Consider logging the error, providing an appropriate message, and allowing the script to continue or exit gracefully.
+8. Encapsulate functionality in functions: Break down the script into smaller, reusable functions. This will improve code modularity, readability, and reusability. Each function should have a single responsibility, making the code easier to understand and maintain.
+9. Add docstrings and comments: Add descriptive docstrings to functions and comments where necessary to explain the purpose and behavior of the code. This will help other developers (including your future self) understand the script more easily.
+10. Consider using command-line arguments: Instead of hardcoding the paths and filenames in the script, you could consider using command-line arguments. This would make the script more flexible and allow users to specify the required paths and filenames when executing the script.
+11. Consider using a configuration file or environment variables: Instead of hardcoding configuration values directly in the script, you might want to consider using a separate configuration file (e.g., JSON, YAML) or environment variables. This makes it easier to modify the script's behavior without modifying the code itself.
+
+Please make sure to replace /path/to/log_directory/ with the actual path to your desired log directory. Additionally, verify and adjust any other paths and filenames according to your needs.
+
+Remember to test the updated script with your specific use case and ensure it behaves as expected.
 
 ### Author
 - [Graham Pinkston](https://github.com/avelis26)
--- Vilicus.py
+++ Vilicus.py
@@ -6,133 +6,137 @@ import time
 import os
 import ffmpeg
 import decimal
-total_before_filesize = []
-total_after_filesize = []
-conversion_counter = 0
+
+LOG_PARENT_PATH = '/path/to/log_directory/'  # Replace with your desired log directory path
+PARAMETERS_FILE = 'parameters.json'
+MOVIES_MANIFEST_FILE = 'movies_manifest.txt'
+EXIT_FILE = 'exit_file.txt'
+
+def load_parameters():
+    try:
+        with open(PARAMETERS_FILE) as f:
+            parameters = json.load(f)
+        return parameters
+    except Exception as e:
+        logging.error(f"Failed to load parameters: {e}")
+        sys.exit(1)
+
+def setup_logging(ops_log):
+    try:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="%(asctime)s [%(levelname)s] %(message)s",
+            handlers=[
+                logging.FileHandler(ops_log),
+                logging.StreamHandler(sys.stdout)
+            ]
+        )
+        logging.info('******************************************************')
+        logging.info('EXECUTION START')
+        logging.debug(f'ops_log: {ops_log}')
+    except Exception as e:
+        logging.error(f"Failed to set up logging: {e}")
+        sys.exit(1)
+
+def create_movies_manifest(parameters):
+    try:
+        input_path = parameters['movies_parent_path']
+        movies_manifest_path = os.path.join(LOG_PARENT_PATH, MOVIES_MANIFEST_FILE)
+
+        if os.path.exists(movies_manifest_path):
+            os.remove(movies_manifest_path)
+
+        movie_list = []
+        for current_path, _, file_names in os.walk(input_path):
+            for file_name in file_names:
+                file_path = os.path.join(current_path, file_name)
+                file_size = os.path.getsize(file_path)
+                if file_size > parameters['min_file_size']:
+                    try:
+                        probe_output = ffmpeg.probe(file_path)
+                        for stream in probe_output['streams']:
+                            if stream['codec_type'] == 'video' and stream['codec_name'] != 'hevc':
+                                print(file_name)
+                                movie_list.append(file_path)
+                    except ffmpeg.Error as e:
+                        logging.error(f"Failed to probe file: {file_path}")
+                        logging.error(e.stderr)
+
+        movie_set = set(movie_list)
+        logging.info('Total Non-h265 Movies: ' + str(len(movie_set)))
+        with open(movies_manifest_path, 'w') as f:
+            for movie in movie_set:
+                f.write(movie + '\n')
+
+        logging.info('Non-h265 movie manifest created.')
+        return movies_manifest_path
+    except Exception as e:
+        logging.error(f"Failed to create movies manifest: {e}")
+        sys.exit(1)
+
+def soft_exit(exit_file_path):
+    if os.path.exists(exit_file_path):
+        logging.info('EXECUTION STOPPED BY USER')
+        logging.info('******************************************************')
+        sys.exit()
+
+def convert_to_h265(source_file_path):
+    try:
+        base = os.path.splitext(source_file_path)[0]
+        output_file = base + '.mkv'
+        logging.debug(output_file)
+
+        os.rename(source_file_path, source_file_path + '.old')
+        ffmpeg.input(source_file_path + '.old').output(output_file, vcodec="libx265", crf=28, acodec="copy").run()
+        time.sleep(2)
+        ffmpeg.input(output_file).output("null", f="null").run()
+        logging.info("Video validation succeeded.")
+
+        before_file_size = os.path.getsize(source_file_path + '.old')
+        after_file_size = os.path.getsize(output_file)
+        total_difference = before_file_size - after_file_size
+        total_before_filesize.append(before_file_size)
+        total_after_filesize.append(after_file_size)
+        space_saved = decimal.Decimal(sum(total_before_filesize) - sum(total_after_filesize)) / decimal.Decimal(1073741824)
+        space_saved = round(space_saved, 2)
+
+        logging.debug('Before Size: ' + str(before_file_size))
+        logging.debug('After Size:  ' + str(after_file_size))
+        logging.debug('Difference:  ' + str(total_difference))
+
+        os.remove(source_file_path + '.old')
+
+        logging.info(f'Conversions complete: {conversion_counter}')
+        logging.debug('Total Diff:  ' + str(total_difference))
+        logging.info('Gigabytes saved: ' + str(space_saved) + ' GBs')
+    except Exception as e:
+        logging.error(f"Failed to convert file: {source_file_path}")
+        logging.error(e)
+        sys.exit(1)
 
 def main():
-	global conversion_counter
-	# Load parameters from json file and set vars.
-	try:
-		parameters = json.load(open('parameters.json'))
-		input_path = parameters['movies_parent_path']
-		opsLog = parameters['log_parent_path'] + parameters['log_filename']
-		movies_manifest_path = parameters['log_parent_path'] + parameters['movies_manifest_filename']
-		exitFile = parameters['log_parent_path'] + parameters['exit_filename']
-	except Exception as e:
-		print("ERROR01: ",e)
-		exit()
-	# Create working directory ~/vilicus/ if not exist, delete manifest if exists.
-	try:
-		if os.path.exists(parameters['log_parent_path']):
-			if os.path.exists(movies_manifest_path):
-				os.remove(movies_manifest_path)
-		else:
-			os.mkdir(parameters['log_parent_path'])
-	except Exception as e:
-		print("ERROR02: ",e)
-		exit()
-	# Set up logging to terminal and file.
-	try:
-		logging.basicConfig(
-			level=logging.DEBUG,
-			format="%(asctime)s [%(levelname)s] %(message)s",
-			handlers=[
-				logging.FileHandler(opsLog),
-				logging.StreamHandler(sys.stdout)
-			]
-		)
-		logging.info('******************************************************')
-		logging.info('EXECUTION START')
-		logging.debug('input_path:           ' + input_path)
-		logging.debug('movies_manifest_path: ' + movies_manifest_path)
-		logging.debug('opsLog:               ' + opsLog)
-		logging.debug('exitFile:             ' + exitFile)
-		logging.info('Creating non-h265 movie manifest...')
-	except Exception as e:
-		logging.error("ERROR03: " + str(e))
-		exit()
-	# Create non-h265 movie manifest.
-	try:
-		movie_list = []
-		for current_path, directories, file_names in os.walk(input_path):
-			for file_name in file_names:
-				file_size = os.path.getsize(current_path + '/' + file_name)
-				if file_size > parameters['min_file_size']:
-					try:
-						probe_output = ffmpeg.probe(current_path + '/' + file_name)
-						for stream in probe_output['streams']:
-							if (stream['codec_type'] == 'video'):
-								if (stream['codec_name'] != 'hevc'):
-									print(file_name)
-									movie_list.append(current_path + '/' + file_name)
-					except ffmpeg.Error as e:
-						logging.error("ERROR06: " + (current_path + '/' + file_name))
-						print(e.stderr)
-		movie_set = set(movie_list)
-		logging.info('Total Non-h265 Movies: ' + str(len(movie_set)))
-		with open(movies_manifest_path, 'w') as openFile:
-			for movie in movie_set:
-				openFile.write("%s\n" % movie)
-		logging.info('Non-h265 movie manifest created.')
-	except Exception as e:
-		logging.error("ERROR06: " + str(e))
-		exit()
-	softExit(exitFile)
-	# Read manifest and convert 1 movie at a time.
-	logging.info('Beginning ffmpeg converstions...')
-	manifest_file = open(movies_manifest_path, 'r')
-	lines = manifest_file.readlines()
-	for line in lines:
-		conversion_counter += 1
-		ConvertToH265(line)
-		softExit(exitFile)
-	logging.info('EXECUTION STOP')
-	logging.info('******************************************************')
+    global conversion_counter
+    parameters = load_parameters()
+    ops_log = os.path.join(LOG_PARENT_PATH, parameters['log_filename'])
+    movies_manifest_path = create_movies_manifest(parameters)
+    exit_file_path = os.path.join(LOG_PARENT_PATH, EXIT_FILE)
 
-# Define exit function to gracefully exit if exit file is found.
-def softExit(exitFilePath):
-	if os.path.exists(exitFilePath):
-		logging.info('EXECUTION STOPPED BY USER')
-		logging.info('******************************************************')
-		exit()
+    setup_logging(ops_log)
+    soft_exit(exit_file_path)
 
-# Define function to rename source file, convert to h265, validate, delete source file.
-def ConvertToH265(sourceFilePath):
-	global conversion_counter
-	global total_before_filesize
-	global total_after_filesize
-	sourceFilePath = sourceFilePath.strip()
-	base = os.path.splitext(sourceFilePath)[0]
-	outputFile = base + '.mkv'
-	logging.debug(outputFile)
-	try:
-		os.rename(sourceFilePath, sourceFilePath + '.old')
-		ffmpeg.input(sourceFilePath + '.old').output(outputFile, vcodec="libx265", crf=28, acodec="copy").run()
-		time.sleep(2)
-		ffmpeg.input(outputFile).output("null", f="null").run()
-		logging.info("Video validation succeeded.")
-	except Exception as e:
-		logging.error("ERROR04: " + str(e))
-		exit()
-	try:
-		before_file_size = os.path.getsize(sourceFilePath + '.old')
-		after_file_size = os.path.getsize(outputFile)
-		total_before_filesize.append(before_file_size)
-		total_after_filesize.append(after_file_size)
-		total_difference = sum(total_before_filesize) - sum(total_after_filesize)
-		space_saved = decimal.Decimal(total_difference) / decimal.Decimal(1073741824)
-		space_saved = round(space_saved, 2)
-		logging.debug('Before Size: ' + str(before_file_size))
-		logging.debug('After Size:  ' + str(after_file_size))
-		logging.debug('Difference:  ' + str(before_file_size - after_file_size))
-		os.remove(sourceFilePath + '.old')
-	except Exception as e:
-		logging.error("ERROR05: " + str(e))
-		exit()
-	logging.info('Conversions complete: ' + str(conversion_counter))
-	logging.debug('Total Diff:  ' + str(total_difference))
-	logging.info('Gigabytes saved: ' + str(space_saved) + ' GBs')
+    logging.info('Beginning ffmpeg conversions...')
+    with open(movies_manifest_path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            conversion_counter += 1
+            convert_to_h265(line.strip())
+            soft_exit(exit_file_path)
+
+    logging.info('EXECUTION STOP')
+    logging.info('******************************************************')
 
 if __name__ == "__main__":
-	main()
+    conversion_counter = 0
+    total_before_filesize = []
+    total_after_filesize = []
+    main()
