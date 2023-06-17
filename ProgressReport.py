1be6ea5c2bc667ef172ee8c0492ebf1364d6b09f
Graham Pinkston <graham.pinkston@gmail.com>
2023-06-17 09:26:47 -0500

    Renamed and improved report script.

--- ProgressReport.py
+++ ProgressReport.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 import decimal
 import re
+import json
+
+parameters = json.load(open('parameters.json'))
+opsLog = parameters['log_parent_path'] + parameters['log_filename']
 
 # Extract number from string and add to array.
 total = []
 patrn = "Difference:"
-file = open("/home/avelis/vilicus/vilicus.log", "r")
+file = open(opsLog, "r")
 for line in file:
 	if re.search(patrn, line):
 		list = line.split()
 		num = int(list[-1])
 		total.append(num)
 		print(line)
-space_saved = decimal.Decimal(sum(total)) / decimal.Decimal(1073741824)
-space_saved = str(round(space_saved, 2))
+space_saved = str(round((decimal.Decimal(sum(total)) / decimal.Decimal(1073741824)), 2))
 print("Total Space Saved: " + space_saved + " GBs")
+print("Total Conversions: " + str(len(total)))
 file.close()
