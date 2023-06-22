aa7c11edf4a827b68ddc57ae32e66d00be3843a7
Graham Pinkston <graham.pinkston@gmail.com>
2023-06-22 12:43:16 -0500

    Script finding .old files.

--- Find_Old_Files.py
+++ Find_Old_Files.py
@@ -1,16 +1,31 @@
 import os
+import sys
 
-def find_files(directory, extension):
-    result = []
-    for root, _, files in os.walk(directory):
-        for file in files:
-            if file.endswith(extension):
-                result.append(os.path.join(root, file))
-    return result
+def main(directory, extension):
+    global conflicts
+    global counter
+    try:
+        for root, _, files in os.walk(directory):
+            for file in files:
+                if file.endswith(extension):
+                    old_file = os.path.join(root, file)
+                    new_file = os.path.join(root, file.replace(extension, ''))
+                    print(old_file)
+                    print(new_file)
+                    if os.path.exists(new_file):
+                        conflicts += 1
+                        print('CONFLICT')
+                    print('\n')
+                    counter += 1
+                    #os.rename(old_file, new_file)
+        print(counter)
+        print(conflicts)
+    except:
+        sys.exit(1)
 
-# Example usage
-directory = '/mnt/data/Media/Movies/'
-extension = '.old'
-files = find_files(directory, extension)
-for file in files:
-    print(file)
+if __name__ == "__main__":
+    counter = 0
+    conflicts = 0
+    directory = '/mnt/data/Media/Shows/'
+    extension = '.old'
+    main(directory, extension)
