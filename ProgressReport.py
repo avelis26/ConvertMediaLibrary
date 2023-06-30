31d68d62bba6286b8e803f349e342b91dcdda0f3
Graham Pinkston <graham.pinkston@gmail.com>
2023-06-30 07:21:09 -0500

    Updated formatting of report.

--- ProgressReport.py
+++ ProgressReport.py
@@ -7,11 +7,11 @@ import re
 parser = argparse.ArgumentParser(description='Analyze log and report metrics.')
 parser.add_argument('-p', '--paramfile', nargs='+', help='Path to the input parameters file', required=True)
 args = parser.parse_args()
+total_conversions = []
 total_space_saved = []
 for paramfile in args.paramfile:
 	parameters = json.load(open(paramfile))
 	opsLog = parameters['log_parent_path'] + parameters['log_filename']
-	# Extract number from string and add to array.
 	total = []
 	patrn = "Difference:"
 	file = open(opsLog, "r")
@@ -20,10 +20,14 @@ for paramfile in args.paramfile:
 			list = line.split()
 			num = int(list[-1])
 			total.append(num)
-			print(line)
 	space_saved = round((decimal.Decimal(sum(total)) / decimal.Decimal(1073741824)), 2)
 	total_space_saved.append(space_saved)
-	print("Total Space Saved: " + str(space_saved) + " GBs")
-	print("Total Conversions: " + str(len(total)))
+	total_conversions.append(len(total))
+	print(f"-{paramfile}")
+	print(f"--Space Saved: {space_saved} GBs")
+	print(f"--Conversions: {len(total)}")
+	print("-")
 	file.close()
-print("Grand Total Space Saved: " + str(sum(total_space_saved)) + " GBs")
+print(f"Total Space Saved: {str(sum(total_space_saved))} GBs")
+print(f"Total Conversions: {str(sum(total_conversions))}")
+
